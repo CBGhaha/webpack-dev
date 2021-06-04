fc179d0ea5614133b76d6ae49f6ec656b5e6aab9
CBGhaha <875675253@qq.com>
2021-06-04 19:56:11 +0800

    dist

---
+++ demo/dist/async2.js
@@ -0,0 +1,15 @@
+(window['webpackJsonp'] = window['webpackJsonp'] || []).push([['async2'], {
+  
+    './src/async2.js':
+    (function(module, exports, __webpack_require__) {
+      const name = __webpack_require__("./src/name.js");
+
+module.exports = name;
+    }),
+  
+    './src/name.js':
+    (function(module, exports, __webpack_require__) {
+      module.exports = 'bangguo';
+    }),
+  
+}]);
